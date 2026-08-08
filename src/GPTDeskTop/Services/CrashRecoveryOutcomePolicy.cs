bc5f085f74d9207844849315ab53b9a73c71f69d
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-09 02:03:36 +0300

    test: add crash recovery outcome policy

---
+++ src/GPTDeskTop/Services/CrashRecoveryOutcomePolicy.cs
@@ -0,0 +1,17 @@
+namespace GPTDeskTop.Services;
+
+public enum CrashRecoveryOutcome
+{
+    Success,
+    SendFailed,
+    Cancelled
+}
+
+public static class CrashRecoveryOutcomePolicy
+{
+    public static bool ShouldStartMonitor(CrashRecoveryOutcome outcome, bool enabled) =>
+        enabled && outcome == CrashRecoveryOutcome.Success;
+
+    public static bool ShouldClearPending(IReadOnlyCollection<CrashRecoveryOutcome> outcomes) =>
+        outcomes.Count > 0 && outcomes.All(x => x == CrashRecoveryOutcome.Success);
+}
