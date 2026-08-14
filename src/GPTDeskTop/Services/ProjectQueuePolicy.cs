d470fcd2e9f6aa1c6a5338e3501cfd9cfde3563c
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-14 03:34:53 +0300

    fix(rel): align project queue policy with queued task model

--- src/GPTDeskTop/Services/ProjectQueuePolicy.cs
+++ src/GPTDeskTop/Services/ProjectQueuePolicy.cs
@@ -1,5 +1,5 @@
 namespace GPTDeskTop.Services;
 public static class ProjectQueuePolicy
 {
-    public static IEnumerable<ProjectQueueItem> Order(IEnumerable<ProjectQueueItem> items) => items.OrderByDescending(x => x.Priority).ThenBy(x => x.EnqueuedAt).ThenBy(x => x.TaskId, StringComparer.Ordinal);
+    public static IEnumerable<QueuedProjectTask> Order(IEnumerable<QueuedProjectTask> items) => items.OrderByDescending(x => x.Priority).ThenBy(x => x.EnqueuedAt).ThenBy(x => x.TaskId, StringComparer.Ordinal);
 }
