cd724e1cac559df7b2b400173b79fbe7674d920e
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-13 06:38:06 +0300

    test: align rate-limit release identity with v2.0.45

--- tests/GPTDeskTop.RuntimeTests/SimpleMonitorRateLimitOverlayRegressionTests.cs
+++ tests/GPTDeskTop.RuntimeTests/SimpleMonitorRateLimitOverlayRegressionTests.cs
@@ -39,9 +39,9 @@ public sealed class SimpleMonitorRateLimitOverlayRegressionTests
     }
 
     [Fact]
-    public void ReleaseIdentityIsVersion2044()
+    public void ReleaseIdentityIsVersion2045()
     {
         var props = ReadSource("Directory.Build.props");
-        Assert.Contains("<GPTDeskTopVersion>2.0.44</GPTDeskTopVersion>", props, StringComparison.Ordinal);
+        Assert.Contains("<GPTDeskTopVersion>2.0.45</GPTDeskTopVersion>", props, StringComparison.Ordinal);
     }
 }
